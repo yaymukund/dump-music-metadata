fbca41a8a091d14c66bc01f190c965eee82df44c
Mukund Lakshman <yaymukund@gmail.com>
2015-03-07 11:29:06 -0800

    Add nicer counter

--- dump-music-metadata.js
+++ dump-music-metadata.js
@@ -1,40 +1,50 @@
-var getFolders = require('./lib/get-folders'),
+var getUnprocessed = require('./lib/get-unprocessed'),
+    counter = require('./lib/counter'),
     makeFolder = require('./lib/make-folder'),
     makeTrack = require('./lib/make-track'),
     store = require('./lib/store'),
     utils = require('./lib/utils'),
     path = require('path');
 
-var totalCount, processedCount = 0;
-
-var progress = function(msg) {
-  console.log(msg+' '+ (++processedCount) +'/'+totalCount);
-};
-
 var _processFolder = function(dirpath) {
-  var tracks = utils.glob('**/*.@(mp3|flac)', { cwd: dirpath }).then(function(filepaths) {
-    filepaths = filepaths.map(function(p) { return dirpath + '/'+p; });
-    return utils.RSVP.all(filepaths.map(makeTrack));
-  });
+  var opts = { cwd: dirpath },
+      tracks = utils.glob('**/*.@(mp3|flac)', opts).then(function(filepaths) {
+        filepaths = filepaths.map(function(p) {
+          return dirpath + '/'+p;
+        });
+        return utils.RSVP.all(filepaths.map(makeTrack));
+      });
 
-  utils.RSVP.hash({
+  return utils.RSVP.hash({
     folder: makeFolder(dirpath),
     tracks: tracks
   }).then(function(res) {
-    if (!res) {
-      return store.findBy({ name: utils.nameFor(dirpath) });
-    };
-
     res.folder.tracks = res.tracks;
-    progress('Creating track');
+    counter.progress('dirs');
     return store.create(res.folder);
   });
 };
 
-getFolders().then(function(dirpaths) {
-  totalCount = dirpaths.length;
-  console.log('Todo: '+totalCount+' tracks');
+var _processFile = function(filepath) {
+  return utils.RSVP.hash({
+    folder: makeFolder(filepath),
+    track: makeTrack(filepath)
+  }).then(function(res) {
+    res.folder.tracks = [res.track];
+    counter.progress('tracks');
+    return store.create(res.folder);
+  });
+};
+
+getUnprocessed('*/').then(function(dirpaths) {
+  counter.create('dirs', dirpaths.length);
   return dirpaths.map(_processFolder);
 }).then(function() {
   console.log('Finished processing folders');
+  return getUnprocessed('*.@(mp3|flac)');
+}).then(function(filepaths) {
+  counter.create('tracks', filepaths.length);
+  return filepaths.map(_processFile);
+}).then(function() {
+  console.log('Finished processing tracks');
 }).catch(console.log);
